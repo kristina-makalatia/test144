2eadedd6eb8c47fdb85520434cd2c3dff22c6636
kristina-makalatia <kristina.makalatia@geolab.edu.ge>
2023-08-05 12:47:48 +0400

    update link

--- script.js
+++ script.js
@@ -5,6 +5,6 @@
 //   .then((res) => console.log(res.data))
 //   .catch((error) => console.log(error));
 
-fetch("http://localhost:3000/posts")
+fetch("https://kristina-makalatia.github.io/test222/server.json")
   .then((response) => response.json())
   .then((responseData) => console.log(responseData));
