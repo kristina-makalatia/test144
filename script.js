684ee642eba4c82f9f8270401941df718e11d56a
kristina-makalatia <kristina.makalatia@geolab.edu.ge>
2023-08-05 12:42:15 +0400

    update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // import axios from "axios";
 
-axios
-  .get(" http://localhost:3000/posts")
-  .then((res) => console.log(res.data))
-  .catch((error) => console.log(error));
+// axios
+//   .get(" http://localhost:3000/posts")
+//   .then((res) => console.log(res.data))
+//   .catch((error) => console.log(error));
 
-// fetch("https://reqres.in/api/users?page=")
-//   .then((response) => response.json())
-//   .then((responseData) => console.log(responseData));
+fetch("http://localhost:3000/posts")
+  .then((response) => response.json())
+  .then((responseData) => console.log(responseData));
